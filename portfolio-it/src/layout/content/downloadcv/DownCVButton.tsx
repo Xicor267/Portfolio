fe4180f4c7d42e6de8a69e3a80a77a0c91e70640
Nolan.Nguyen <nolan.nguyen@avepoint.com>
2025-08-26 09:15:22 +0700

    Add new download cv buton

--- portfolio-it/src/layout/content/downloadcv/DownCVButton.tsx
+++ portfolio-it/src/layout/content/downloadcv/DownCVButton.tsx
@@ -2,20 +2,29 @@ import { DownloadOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
 import { FC } from 'react';
 import { useLanguage } from '../../../hook/useLanguages';
+import "./DownloadCVButton.scss";
 
 export const DownloadCVButton: FC = () => {
   const cvFilePath = '/CV.pdf';
   const { i18n } = useLanguage();
 
+  const handleShowBtnSmallScreen = () => {
+    if (window.innerWidth < 960) {
+      return ""
+    }
+    i18n("portfolio.page.download.my.cv")
+  }
+
   return (
     <Button
+      className='download-button'
       type="primary"
       icon={<DownloadOutlined />}
       href={cvFilePath}
       download="NGUYEN-PHUONG-NAM-DEV-CV.pdf"
       style={{ marginTop: '0.5rem' }}
     >
-      {i18n("portfolio.page.download.my.cv")}
+      {handleShowBtnSmallScreen()}
     </Button>
   );
 };
